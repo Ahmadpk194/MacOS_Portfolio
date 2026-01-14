816540d02c189cfbdf0f04143a2dbcd230ad02fc
Ahmad khan <ahmadpk194@gmail.com>
2026-01-14 17:37:18 +0500

    implement Welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from '#components/Navbar'
+import Welcome from '#components/Welcome'
 
 function App() {
 
   return (
     <main>
       <Navbar/>
+      <Welcome/>
     </main>
   )
 }

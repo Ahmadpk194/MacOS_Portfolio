b728366079dc92449daf4da9a39d3ee0fa5f4c4e
Ahmad khan <ahmadpk194@gmail.com>
2026-01-14 18:41:43 +0500

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from '#components/Navbar'
-import Welcome from '#components/Welcome'
+import { Dock, Navbar, Welcome } from '#components'
 
 function App() {
 
@@ -11,6 +10,7 @@ function App() {
     <main>
       <Navbar/>
       <Welcome/>
+      <Dock/>
     </main>
   )
 }
